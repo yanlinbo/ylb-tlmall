67b712d1b4d1f54abee45c3c47c0f425af41ebc6
yanlinbo <973664322@qq.com>
2022-11-05 10:45:09 +0800

    严林博 20221105 所有工程启动并注册到nacos上

--- tulingmall-order/src/main/java/com/ylb/order/OrderApplication.java
+++ tulingmall-order/src/main/java/com/ylb/order/OrderApplication.java
@@ -1,4 +1,4 @@
-package com.tlylb.admin;
+package com.ylb.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AdminApplication {
+public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 
 }
